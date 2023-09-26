074ca8243aca717cdc393564e027fcbf8de2b81d
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2023-09-26 00:07:28 -0600

    Add files via upload

---
+++ SandraMorales09 - bootcamp-programacion-practicas/intermedios python/Combat_Continent.py
@@ -0,0 +1,37 @@
+class TangSan:
+
+# Secta Tang el mas destacado
+
+def __init__(self) :
+    print("Miembro y destacado de la secta tang primera vida")
+
+def hability_sect_inter_(self) :
+    print( Purple semon eye, pasos fantasmales y mysterious jade hand):
+    if atacar enemigo o adversario :
+        print("manos de jade misteriosa")
+    else :
+        print("pasos fantasmales")    
+
+def hability_sect_exter_(self) :
+    print("armas ocultas venenosas")
+    if ataque a un enemigo poderoso:
+        print("pluma de pavo real")
+    else :
+        print("loto buda furioso")
+    
+# Dominado Dolou
+
+def __init__(self) :
+   print("Tangsan doble arma espiritual, rencarno en un mundo de habilidades marciales")
+
+def Plant_silver_(self) :
+    print( 5 anillo Lanza Tirano Emperador Azul Plata, 6 anillo Nothingness,instant kill eight stage drop)
+    if debe cultivar:
+        print:("usa artes marciales para combatir usando su 5vo anillo")
+    else: 
+        print:("6vo 100% bloquea el ataque del enemigo y 80% de ataques espirituales")
+
+def Plant_silver_8vo_anillo(self) :
+  print( 8 anillo Destruccion del espejo del espiritu maligno de plata azul y 10 anillo Poseidon)
+   if  examen del dios de mar:
+       print: ("absorcion del diablo de la orca plateada cristal atrapa a multiples oponentes" )
