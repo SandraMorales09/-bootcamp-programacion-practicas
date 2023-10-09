6cf27c16cf7ab07cd66f58140867b3e450141e2a
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2023-10-09 00:12:14 -0600

    Add files via upload

---
+++ SandraMorales09 - bootcamp-programacion-practicas/intermedios python/Combat_Continent.py
@@ -0,0 +1,115 @@
+class Secttang(xiaosan,tangsan):
+
+# Secta Tang el tang ssn prodigio de la secta
+
+def __init__(self) :
+    print("Miembro y destacado de la secta tang primera vida")
+
+def hability_sect_inter_tecnicas(self) :
+    print( Purple semon eye, pasos fantasmales y mysterious jade hand):
+    if atacar enemigo o adversario :
+        print("manos de jade misteriosa")
+    else :
+        print("pasos fantasmales")    
+
+def hability_sect_exter_(self) :
+    print("armas ocultas venenosas")
+    if ataque a un enemigo poderoso:
+        print("pluma de pavo real")
+    else :
+        print("loto buda furioso")
+    
+# Dominado Dolou
+
+def __init__(self) :
+   print("Tangsan doble arma espiritual, rencarno en un mundo de habilidades marciales")
+
+def Plant_silver_(self) :
+    print( 5 anillo Lanza Tirano Emperador plant silver, 6 anillo Nothingness,instant kill eight stage drop)
+    if debe cultivar:
+        print:("usa artes marciales para combatir usando su 5vo anillo")
+    else: 
+        print:("6vo 100% bloquea el ataque del enemigo y 80% de ataques espirituales")
+
+def Plant_silver_8vo_anillo(self) :
+  print( 8 anillo Destruccion del espejo de espiritu maligno de silver plant y 10 anillo Poseidon)
+   if  examen del dios de mar:
+       print: ("crea cristal atrapa a multiples oponentes cristaliza agua o aire, incluso el cuerpo y la sangre del oponente" )
+   else:
+       print:("10 anillo nivel 98 dominado doluo poseidon ring sea god divine sea god devour livera una luz  dorada un remolino de 3 metros")
+
+def Hao_tian_(self) :
+  print:(6 anillo Earth ant emperor slash divide la tierra, suelo y rocas, 10 anillo dios asura y 7 anillo avatar de martillo de cielo)
+  if un ataque directo y directo:
+        print: ("dividir con facilidad tierra, suelo, rocas y 10 anillo asura un aura fria con una presion abismal domio de la muerte espada de sangre ")
+  else:
+        print: ("crea un avatar la silueta roja de tang san y el hao tian aumenta tamaño y peso")
+
+def hueso_espiritual(self) :
+   print:(eight spider lances de 2000 años, skull bone mind condensing wisdom 60.000 años, left arm bone titan giant ape 100,000 años y right leg bone blue silver emperor 100,000 años)
+   if un ataque furtivo:
+        print: ("8 lanzas de araña drena la enegia espriritual, hueso del titan simio control de gravedad titan firmament breaker comprime el aire en una energia aterradora")
+   else:
+        print:("right leg bone blue silver emperor habilidad primaveral curativa al usuaria y aliados, 2 habilidad vuelo")
+        
+class TangSan(Dominadodolou):
+
+    def __init__(self) :
+        print("Tang san reencarna en la tierra de dominados dolou")
+        print("Tang san en especial nace con doble arma espiritual y con sus habilidades de sus vida pasada")
+      
+    def plant_silver(self) :
+        print(" necesita usar 9 anillo alma del dragon Celestial plant silver")
+        return  9 anillo().Desintegra_todo_lo_que_toca_() # no deja nada atras
+
+    def usa_purple_demon_eye(self):
+            print("purple demon eye")
+
+ class Tangsan(dominado dolou, Tansan):
+    def __init__(self):
+        print("2 alma espiritual Clear sky hammer")       
+
+    def dark_demon_thunderous_fury(self):   
+        print("Gira y lanza su martillo")   
+        return lanza().martillo_con_una_fueza() # contacto gigantesco explosion de relampagos
+
+class Tansan(Secttang, dominado dolou):
+
+    def __init__(self):
+        print("obtiene el 10anillo divino de sea god y 10anillo divino de dios asura")
+        return dolou().millenial_space_envuelve_a_los_oponentes() # en una nube dorada, seguida de una explosion
+
+ san = tangsan()
+ san.pasos_fantasmales() 
+
+ san = tangsan()
+ san.loto_buda_furioso()
+
+ san = tangsan()
+ san.plant_silver_5_anillo_lanza_emperador_tirano()
+
+ san = tangsan()
+ san.hao_tian_6_anillo_Earth_ant_emperor_slash_divide_objetos()
+
+ san = tansan()
+ san.8_lanzas_de_araña_2000_años()
+
+san = tangsan()
+san.poseidon_ring_sea_god_divine() # esta funcion viene de Tangsan, anterior dios del mar
+
+san = tangsan()
+san.asura_ring_judgement_gog_divine() # esta funcion viene de Tangsan, otorgado por el anterior dios asura
+
+tangsan.pluma_pavo_real(loto de buda furioso=True)
+
+print(isinstance(blue silver plant, Hao tian, armas ocultas, tecnicas, hueso espiritual  Tangsan ))
+
+
+
+
+
+
+
+
+
+
